9d526607f69dafdf4b03727c509ce37c6f2cedcb
mackykky <164979941+mackykky@users.noreply.github.com>
2025-07-31 12:34:38 +0800

    push

--- api/axiosIntance.ts
+++ api/axiosIntance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://10.252.69.223:3000/ridepay', 
+  baseURL: 'http://192.168.100.7:3000/ridepay', 
   timeout: 10000,
 });
 
